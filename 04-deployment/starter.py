b2cbfa5e6ec2ca71d54982c183b965682dc139c1
Duc Ho <hoduc1202@gmail.com>
2025-05-20 01:18:34 +0000

    Add homework 04

---
+++ 04-deployment/starter.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys
+import pickle
+import pandas as pd
+import numpy as np
+
+
+with open('model.bin', 'rb') as f_in:
+    dv, model = pickle.load(f_in)
+
+
+categorical = ['PULocationID', 'DOLocationID']
+
+def read_data(filename):
+    df = pd.read_parquet(filename)
+
+    df['duration'] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
+    df['duration'] = df.duration.dt.total_seconds() / 60
+
+    df = df[(df.duration >= 1) & (df.duration <= 60)].copy()
+
+    df[categorical] = df[categorical].fillna(-1).astype('int').astype('str')
+
+    return df
+
+
+
+
+def run(year, month):
+    taxi_type = "yellow"
+    input_file = f'https://d37ci6vzurychx.cloudfront.net/trip-data/{taxi_type}_tripdata_{year:04d}-{month:02d}.parquet'
+    output_file = f'output/{taxi_type}/{year:04d}-{month:02d}.parquet'
+
+
+    df = read_data(input_file)
+
+    dicts = df[categorical].to_dict(orient='records')
+    X_val = dv.transform(dicts)
+    y_pred = model.predict(X_val)
+
+    print(f"mean pred:{np.mean(y_pred):.2f}")
+
+
+    df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
+
+
+    df_result = pd.DataFrame()
+    df_result['ride_id'] = df['ride_id']
+    df_result['predicted_duration'] = y_pred
+    df_result.to_parquet(
+        output_file,
+        engine='pyarrow',
+        compression=None,
+        index=False
+    )
+
+if __name__ == "__main__":
+    run(
+        int(sys.argv[1]),
+        int(sys.argv[2])
+    )
